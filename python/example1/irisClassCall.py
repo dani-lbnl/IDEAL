12b546b5e11b661dc17e2e0cb39b032a24549736
Dani Ushizima <dani.lbnl@gmail.com>
2017-04-11 05:57:58 -0700

    moving the sensitive

--- python/example1/irisClassCall.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bin Python2.7.11
-import irisClass as ic
-import sys
-
-hoursOfSleep = sys.argv[1]
-print 'Numbers of hour she slept=' + hoursOfSleep
-today = ic.Iris(hoursOfSleep)
-today.greetings()
